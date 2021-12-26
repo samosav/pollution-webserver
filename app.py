decd20cdb048027419a92c99952c60ec2773dd94
samosav <samosathegrandma@gmail.com>
2021-12-26 13:52:46 +0100

    get results from pollution sensor and write to file

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
- # app.py
+from datetime import datetime
 
 from flask import Flask, request
+
 app = Flask(__name__)
 
-app.DATA=None
 
-@app.route('/')
-def show_data():
-    return f'<h1>Test Server Data</h1>{app.DATA}'
+app.route('/')
+def index():
+    return 'OK'
 
 @app.route('/update')
 def update_data():
-    app.DATA = request.args.get('d')
-    print(app.DATA)
-    return f'<h1>Test Server Data</h1>{app.DATA}'
+    val = request.args.get('d')
+    timestamp = datetime.utcnow()
+    with open("results2.csv","a") as outputfile:
+        outputfile.write(f"{timestamp},{val}\n")
+    return str(val)
 
 # We only need this for local development.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0')
 
